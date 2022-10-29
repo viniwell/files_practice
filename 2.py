5e4634e13662d3d6789f650dc125bc3fb5b9a1e4
viniwell <viniwell2807@gmail.com>
2022-10-29 16:20:37 +0100

    2.py

---
+++ 2.py
@@ -0,0 +1,8 @@
+
+def calc_max():
+    with open('file', 'r', encoding='UTF-8') as file:
+        a=[int(x) for x in list(file.readlines())]
+    with open('file', 'a', encoding='UTF-8') as file:
+        file.write(str(sum(a))+'\n')
+        file.write(str(max(a))+'\n')
+calc_max()
